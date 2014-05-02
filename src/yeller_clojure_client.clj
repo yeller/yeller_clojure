fc84cefa7328c66bc9054aa101d00ed52a866d67
Tom Crayford <tcrayford@googlemail.com>
2014-05-02 13:27:47 +0100

    actually implement error handler

--- src/yeller_clojure_client.clj
+++ src/yeller_clojure_client.clj
@@ -1,20 +1,50 @@
 (ns yeller-clojure-client
   (:require [clojure.walk :refer [stringify-keys]])
-  (:import (com.yellerapp.client YellerHTTPClient YellerClient YellerExtraDetail)))
+  (:import (com.yellerapp.client
+             YellerHTTPClient
+             YellerClient
+             YellerExtraDetail)))
+
+(defn default-io-error-handler [backend error]
+  (.println *err* (str "Yeller: an io error ocurred whilst talking to yeller: " error)))
+
+(defn default-auth-error-handler [backend error]
+  (.println *err* (str "Yeller: an authentication error ocurred whilst talking to yeller: " error)))
 
 (defn ^YellerClient client
   "creates a new client from a map of configuration settings Required settings:
-   {:token \"YOUR API KEY HERE\"}
-   other settings:
-   :error-handler (fn [error] (println error))
-   a function for reporting errors when communicating with yeller's servers
+  {:token \"YOUR API KEY HERE\"}
+
+  other settings:
+  :auth-error-handler (fn [backend error] (println \"Yeller: an authentication error occurred whilst talking to yeller:\" backend error))
+  a function for reporting authentication errors when communicating with
+  yeller's servers. You might want this to print out to a log file (by default
+  it prints to stderr). Takes a String for the hostname of the api server it
+  was talking to during the error, and a Throwable for whatever exception
+  ocurred.
+  :io-error-handler (fn [backend error] (println \"Yeller: an io error occurred whilst talking to yeller:\" backend error))
+  a function for reporting io errors when communicating with
+  yeller's servers. You might want this to print out to a log file (by default
+  it prints to stderr). Takes a String for the hostname of the api server it
+  was talking to during the error, and a Throwable for whatever exception
+  ocurred.
   "
   [options]
   (assert (string? (:token options)) "Yeller client must be passed your api key as a string under :token")
   (let [client (YellerHTTPClient. (:token options))]
-    client
-    )
-  )
+    (if (or (:io-error-handler options)
+            (:auth-error-handler options))
+      (.setErrorHandler
+        client
+        (reify
+          YellerErrorHandler
+          (reportAuthError [backend e]
+            ((:auth-error-handler options default-auth-error-handler)
+               backend e))
+          (reportIOError [backend e]
+            ((:auth-io-handler options default-auth-error-handler)
+               backend e)))))
+    client))
 
 (defn ^YellerExtraDetail add-url [^YellerExtraDetail detail extra]
   (if (string? (:url extra))
