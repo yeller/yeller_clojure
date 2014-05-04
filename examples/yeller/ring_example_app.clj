a7a23c53b423947065b91291bc2f9fe8ccad8105
Tom Crayford <tcrayford@googlemail.com>
2014-05-04 17:08:37 +0100

    wrap the sample app in session/params

--- examples/yeller/ring_example_app.clj
+++ examples/yeller/ring_example_app.clj
@@ -1,10 +1,14 @@
 (ns yeller.ring-example-app
   (:require [ring.adapter.jetty9 :as jetty]
+            [ring.middleware.params :as params]
+            [ring.middleware.session :as session]
             [yeller-clojure-client.ring :as yeller]))
 
 (defn -main [& args]
   (jetty/run-jetty
-    (yeller/wrap-ring
-      (fn [& args] (assert false "error"))
-      {:token "YOUR API KEY HERE"})
+    (params/wrap-params
+      (session/wrap-session
+        (yeller/wrap-ring
+          (fn [& args] (assert false "error"))
+          {:token "YOUR API KEY GOES HERE"})))
     {:port 4001}))
