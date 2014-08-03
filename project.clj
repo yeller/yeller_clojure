81fff78489d6a73149cbcd59848fc8aecd6bfb8e
Tom Crayford <tcrayford@googlemail.com>
2014-08-03 12:46:28 +0100

    bump version, bump java client dep

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject yeller-clojure-client "0.1.0-SNAPSHOT"
+(defproject yeller-clojure-client "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [yeller-java-api "0.0.1-SNAPSHOT"]]
+                 [yeller-java-api "0.0.2-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[info.sunng/ring-jetty9-adapter "0.3.0"]]
                    :source-paths ["examples"]}})
