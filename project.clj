843637b8b67eaf4cdc0d1e522c491e8f5af045ca
Tom Crayford <tcrayford@googlemail.com>
2014-12-08 12:04:28 +0000

    bump java client dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [yeller-java-api "0.0.2-SNAPSHOT"]]
+                 [yeller-java-api "0.2.0"]]
   :profiles {:dev {:dependencies [[info.sunng/ring-jetty9-adapter "0.3.0"]]
                    :source-paths ["examples"]}})
