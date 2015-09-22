8e1de0d8dca088f9211ff76d47898e4539f04e0b
Tom Crayford <tcrayford@googlemail.com>
2015-09-21 21:29:19 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject yeller-clojure-client "1.4.0"
+(defproject yeller-clojure-client "1.4.1"
   :description "Clojure client for yellerapp.com"
   :url "https://github.com/tcrayford/yeller_clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [yeller-java-api "1.4.0"]]
+                 [yeller-java-api "1.4.1"]]
   :profiles {:dev {:dependencies [[info.sunng/ring-jetty9-adapter "0.3.0"]]
                    :source-paths ["examples"]}})
