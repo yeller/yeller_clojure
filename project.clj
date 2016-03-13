3b312811c65849e973dd52f3af981f7ead23c3b5
Tom Crayford <tcrayford@googlemail.com>
2016-03-13 21:54:27 +0000

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yeller-clojure-client "1.4.2"
+(defproject yeller-clojure-client "1.4.3"
   :description "Clojure client for yellerapp.com"
   :url "https://github.com/tcrayford/yeller_clojure"
   :license {:name "Eclipse Public License"
